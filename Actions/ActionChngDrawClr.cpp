bb7b3c6dd3fb74bf8302128341c65b7cb62b8c84
mohamed159716 <mohamed159716@yahoo.com>
2022-01-17 17:44:46 +0200

    Added Color Menu

--- Actions/ActionChngDrawClr.cpp
+++ Actions/ActionChngDrawClr.cpp
@@ -16,45 +16,6 @@ void ActionChngDrawClr::Execute()
 	pGUI->PrintMessage("Select A color for drawing and for selecting the default click restore icon ");
 
 	pGUI->setnewDrawColor(pGUI->getAppliedColor());
-
-	//newColor = pManager->GetUserAction();
-
-	//switch (newColor)
-	//{
-	//case CLR_BLUE:
-	//	pGUI->PrintMessage("Now You can draw with blue");
-	//	pGUI->setnewDrawColor(BLUE);
-	//	break;
-
-	//case CLR_BLACK:
-	//	pGUI->PrintMessage("Now You can draw with black");
-	//	pGUI->setnewDrawColor(BLACK);
-	//	break;
-
-	//case CLR_GREEN:
-	//	pGUI->PrintMessage("Now You can draw with green");
-	//	pGUI->setnewDrawColor(GREEN);
-	//	break;
-
-	//case CLR_YELLOW:
-	//	pGUI->PrintMessage("Now You can draw with yellow");
-	//	pGUI->setnewDrawColor(YELLOW);
-	//	break;
-
-	//case CLR_RED:
-	//	pGUI->PrintMessage("Now You can draw with red");
-	//	pGUI->setnewDrawColor(RED);
-	//	break;
-
-	//case default_setting:
-	//	pGUI->PrintMessage("Now You can draw with default color");
-	//	pGUI->setnewDrawColor(BLACK);
-	//	break;
-
-	//default:
-	//	break;
-	//}
-	
 	
 }
 
--- Actions/ActionChngFillClr.cpp
+++ Actions/ActionChngFillClr.cpp
@@ -1,6 +1,7 @@
 #include "ActionChngFillClr.h"
 #include "..\ApplicationManager.h"
 #include "..\GUI\GUI.h"
+#include <iostream>
 
 
 
@@ -15,54 +16,6 @@ void ActionChngFillClr::Execute()
 {
 	GUI* pGUI = pManager->GetGUI();
 
-	pGUI->PrintMessage("Select A color for filling and for no filling select restore icon ");
-
-	newColor = pManager->GetUserAction();
-
-	switch (newColor)
-	{
-	case CLR_BLUE:
-		UI.isFilled = true;
-		pGUI->setnewFillColor(BLUE);
-		pGUI->PrintMessage("Now you can draw shape filled by blue");
-
-		break;
-
-	case CLR_BLACK:
-		UI.isFilled = true;
-		pGUI->setnewFillColor(BLACK);
-		pGUI->PrintMessage("Now you can draw shape filled by black");
-
-		break;
-
-	case CLR_GREEN:
-		UI.isFilled = true;
-		pGUI->setnewFillColor(GREEN);
-		pGUI->PrintMessage("Now you can draw shape filled by green");
-		break;
-
-	case CLR_YELLOW:
-		UI.isFilled = true;
-		pGUI->setnewFillColor(YELLOW);
-		pGUI->PrintMessage("Now you can draw shape filled by yellow");
-
-		break;
-
-	case CLR_RED:
-		UI.isFilled = true;
-		pGUI->setnewFillColor(RED);
-		pGUI->PrintMessage("Now you can draw shape filled by red");
-		break;
-
-	case default_setting:
-		UI.isFilled = false;
-		pGUI->PrintMessage("No fill");
-		break;
-
-	default:
-		break;
-	}
-
-
+	pGUI->setnewFillColor(pGUI->getAppliedColor());
 }
 
